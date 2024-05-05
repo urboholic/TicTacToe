e2be458e062a73e3b1aa9ff597d85300143b8eb1
Maxime Spendrup <spendrup.design@gmail.com>
2024-05-05 11:30:21 +0200

    Started working on the GameBoard window

---
+++ TicTacToeBL/ViewModel/GameBoardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeBL.ViewModel
+{
+    public class GameBoardViewModel
+    {     
+        public GameBoardViewModel()
+        {
+        }
+    }
+}
---
+++ TicTacToeGUI/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TicTacToeGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
