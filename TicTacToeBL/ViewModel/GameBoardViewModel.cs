b2a42e02620bd471590072d671465d4b3950a492
Maxime Spendrup <spendrup.design@gmail.com>
2024-05-05 20:41:22 +0200

    Added relaycommand and buttons to the grid

--- TicTacToeBL/ViewModel/GameBoardViewModel.cs
+++ TicTacToeBL/ViewModel/GameBoardViewModel.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using TicTacToeBL.Command;
 
 namespace TicTacToeBL.ViewModel
 {
     public class GameBoardViewModel
-    {     
+    {
+        private RelayCommand<object>? _PressTestCmd;
+        public RelayCommand<object> PressTestCmd => _PressTestCmd ??= new RelayCommand<object>(PressTest1);
         public GameBoardViewModel()
         {
+
+        }
+        public void PressTest1(object parameter)
+        {
+            MessageBox.Show("Pressed The Button");
         }
     }
 }
